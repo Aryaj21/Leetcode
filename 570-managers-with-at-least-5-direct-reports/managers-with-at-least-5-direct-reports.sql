691b0cff9d1be540c0af1a1468b5fe90af60ef6e
Aryaj21 <147510659+Aryaj21@users.noreply.github.com>
2026-09-14 02:00:56 +0530

    Time: 352 ms (81.73%) | Memory: 0B (100.00%) - LeetSync

---
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT e1.name
+FROM Employee e1 JOIN(
+SELECT managerId, COUNT(*) AS directReports
+FROM Employee
+GROUP BY managerId
+HAVING COUNT(*)>=5    
+) e2
+ON e2.managerId=e1.id
